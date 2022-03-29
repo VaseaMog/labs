26402d8ce5095dbf6ef4fc4ba046a01ec954a2c3
VaseaMog <98794538+VaseaMog@users.noreply.github.com>
2022-03-29 09:25:17 +0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-int main() 
+int main()
 {
-	double x;
-	printf("Enter x [3 <= x <= 5]\n");
-	scanf ("%lf", &x);
-	if (3.0 <= x && x <= 5.0)
-	{
-		double y = asin(4 - x) + log(log(x));
-		double z = acos((2*y)/(1+y));
-		printf("y(x) = %lf\nz(y) = %lf\n", y, z);
-	}
-	else
-		printf("x value is incorrect !\n");
-	return 0;
+    double x;
+    printf("Enter x [3.27 <= x <= 4.68]\n");
+    scanf ("%lf", &x);
+    if (3.27 <= x && x <= 4.68)
+    {
+        double y = asin(4 - x) + log(log(x));
+        double z = acos((2*y)/(1+y));
+        printf("y(x) = %lf\nz(y) = %lf\n", y, z);
+    }
+    else
+        printf("x value is incorrect !\n");
+    return 0;
 }
