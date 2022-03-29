e4230754cc18105d08386f2f3df7adf77cbfbb4f
VaseaMog <98794538+VaseaMog@users.noreply.github.com>
2022-03-29 08:09:18 +0500

    Create rek.c

---
+++ rek.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <iostream>
+//рекурсия
+int ra;
+int la;
+int l;
+unsigned  int fl(unsigned  int r)
+{
+    while ()
+}
+int main()
+{
+unsigned int r;
+printf("Enter r ->\n");
+scanf("%u", &r);
+unsigned int l;
+printf( "Enter l ->\n");
+scanf("%u", &l);
+printf("Recursion: = %u\n", fl(l));
+return 0;
+}
