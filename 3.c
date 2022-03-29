23b3a67d69407262a4c716e95b2f664264a0a6d7
VaseaMog <98794538+VaseaMog@users.noreply.github.com>
2022-03-29 08:43:33 +0500

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x, h, f;
+    printf("Vvedite x\n");
+    scanf("%lf", &x);
+    while (x < 0 || x > 3) {
+        printf("nevernoe znachenie X, vvedite 0 <= x <= 3\n");
+        scanf("%lf", &x);
+    }
+    printf("Vvedite h\n");
+    scanf("%lf", &h);
+
+    printf("x          f(x)\n");
+    printf("_______________\n");
+    while (0 <= x && x <= 1.5) {
+        f = pow(2.0, x) - 2.0 + x * x;
+        printf("%.1lf          %.1lf\n", x, f);
+        x = x + h;
+        if (x > 1.5)
+            return 0;
+    }
+    while (1.5 < x && x <= 3.0) {
+        f = sqrt(x) * pow(M_E, -x*x);
+        printf("%.1lf          %.1lf\n", x, f);
+        x = x + h;
+    }
+}
