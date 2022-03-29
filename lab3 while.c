429de55c9c7c04fd3831d9988c1c60aeec5a1a7c
VaseaMog <98794538+VaseaMog@users.noreply.github.com>
2022-03-29 09:06:01 +0500

    Update lab3 while.c

--- lab3 while.c
+++ lab3 while.c
@@ -2,23 +2,23 @@
 #include <math.h>
 int main()
 {
-	double y, h;
-	printf("Enter h -> ");
-	scanf("%lf", &h);
-	printf("x       f(x)\n------------\n");
-	double x = 0.00;
-	while (x <= 2.0)
-	{
-		if (x < 1.0)
-		{
-			y = x * x * atan(x);
-			printf("%3.2lf     %3.2lf\n", x, y);
-		}
-		else if (x > 1.0)
-		{
-			y = sin((1.0 / (x * x)));
-			printf("%3.2lf     %3.2lf\n", x, y);
-		}
-	x = x + h;
-	}
+    double y, h;
+    printf("Enter h -> ");
+    scanf("%lf", &h);
+    printf("x       f(x)\n------------\n");
+    double x = 0.0;
+    while (x <= 2.0)
+    {
+        if (x <= 1.0)
+        {
+            y = x * x * atan(x);
+            printf("%3.2lf     %3.2lf\n", x, y);
+        }
+        else if (x > 1.0)
+        {
+            y = sin((1.0 / (x * x)));
+            printf("%3.2lf     %3.2lf\n", x, y);
+        }
+        x = x + h;
+    }
 }
