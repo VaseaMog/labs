1799921d8c13c9ae24e0d6a49ec345bf356e84ab
VaseaMog <98794538+VaseaMog@users.noreply.github.com>
2022-04-09 12:57:50 +0500

    Update lab3_na_pokaz.c

--- lab3_na_pokaz.c
+++ lab3_na_pokaz.c
@@ -27,3 +27,30 @@ int main()
     }
     return 0;
 }
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int b;
+    double y, h;
+    printf("Enter h -> ");
+    scanf("%lf", &h);
+    b = 2 / h;
+    printf("x       f(x)\n------------\n");
+    for (double x = 0; b > h; x += h, b--)
+    {
+        if (0 <= x && x <= 1)
+        {
+            y = x * x * atan(x);
+            printf("%3.2lf     %3.2lf\n", x, y);
+        }
+        if (1 < x && x <= 2)
+        {
+            y = sin((1.0 / (x * x)));
+            printf("%3.2lf     %3.2lf\n", x, y);
+        }
+    }
+    return 0;
+}
